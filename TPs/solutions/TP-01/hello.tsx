123589cdff7434e052054ebffdc691cef9b22f3c
jb cazaux <jbcazaux@gmail.com>
2016-08-30 23:21:50 +0200

    TP1

---
+++ TPs/solutions/TP-01/hello.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+export interface Props { name: string;
+}
+
+export class Hello extends React.Component<Props, void> {
+    render() {
+        return <h1>Hello {this.props.name}! {this.toArray(5, 6).join(',')}</h1>;
+    }
+
+    private toArray<T>(arg: T, arg2: T): T[] {
+        return [arg, arg2];
+    }
+}
---
+++ TPs/solutions/TP-01/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Hello } from "./hello";
+
+ReactDOM.render(
+    <Hello name="world"/>,
+    document.getElementById("app")
+);
