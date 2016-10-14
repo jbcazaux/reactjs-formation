53ec78bb4425b40a95d3fa060a929b6ff22f2442
jbcazaux <jbcazaux@gmail.com>
2016-10-14 17:29:27 +0200

    clean src dir

--- src/student.ts
+++
@@ -1,7 +0,0 @@
-export class Student {
-    constructor(public id: number,
-                public lastname: string,
-                public firstname: string,
-                public grades: number[]) {
-    };
-}
